76cb6f15718c2c0c60ed6dde15c58e8e200071e3
Edgar Catalan <edgar@Edgars-MacBook-Pro.local>
2021-10-27 10:10:49 -0700

    first commit

---
+++ js/main.js
@@ -0,0 +1,58 @@
+const collapsibles = document.querySelectorAll(".collapsible");
+collapsibles.forEach((item) =>
+  item.addEventListener("click", function () {
+    this.classList.toggle("collapsible--expanded");
+  })
+);
+
+const zoobleDoc = document.getElementById("zooble-doc");
+const zoobleCode = document.getElementById("zooble-code");
+const zoobleApp = document.getElementById("zooble-app");
+
+const edbDoc = document.getElementById("edb-doc");
+const edbCode = document.getElementById("edb-code");
+
+const ticketPalCode = document.getElementById("ticket-pal-code");
+const ticketPalApp = document.getElementById("ticket-pal-app");
+
+const onlineEditorCode = document.getElementById("code-editor-code");
+const onlineEditorApp = document.getElementById("code-editor-app");
+
+// Zooble events
+zoobleDoc.addEventListener("click", () => {
+  window.open(
+    "https://github.com/edgarjoel18/Zooble-App/tree/development/Milestones"
+  );
+});
+
+zoobleCode.addEventListener("click", () => {
+  window.open(
+    "https://github.com/edgarjoel18/Zooble-App/tree/development/application"
+  );
+});
+
+zoobleApp.addEventListener("click", () => {
+  window.open("https://zoou.app/");
+});
+
+// edb events
+edbDoc.addEventListener("click", () => {
+  window.open(
+    "https://github.com/edgarjoel18/EDB-Hospital-management-database/blob/master/milestones/Milestone2/M2.pdf"
+  );
+});
+
+edbCode.addEventListener("click", () => {
+  window.open(
+    "https://github.com/edgarjoel18/EDB-Hospital-management-database/tree/master/milestones/Milestone3"
+  );
+});
+
+// ticket pal events
+onlineEditorCode.addEventListener("click", () => {
+  window.open("https://github.com/edgarjoel18/online-code-editor");
+});
+
+onlineEditorApp.addEventListener("click", () => {
+  window.open("https://code-editor-nu.vercel.app/");
+});
